dc39a7ffe43ab7933240da8a355a0461c0867cb4
Parker Catalano <catalano.work@gmail.com>
2018-02-14 19:50:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -112,9 +112,10 @@ class ApplicationController < Sinatra::Base
 
   delete '/tweets/:id/delete' do
     @tweet = Tweet.find_by_id(params[:id])
-    @tweet.delete
-
-    redirect '/tweets'
+    if logged_in?
+      @tweet.delete
+      redirect '/tweets'
+    else redirect "/login"
   end
 
   get '/tweets/:id/edit' do
