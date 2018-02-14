ab558aeb109eef8faa915d908bc7761389495000
Parker Catalano <catalano.work@gmail.com>
2018-02-14 17:44:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -92,8 +92,7 @@ class ApplicationController < Sinatra::Base
     if logged_in?
       @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
       binding.pry
-      if true
-
+      if @tweet.valid? == true
         erb :'/tweets/show_tweet'
       else
         redirect '/tweets/new'
